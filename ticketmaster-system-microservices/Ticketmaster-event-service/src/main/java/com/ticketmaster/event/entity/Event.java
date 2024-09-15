c62412357d3940d1a7ce185478b52cd7ff85cafb
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-09-15 07:19:10 +0300

    feat: add Event entity with auditing and validation

---
+++ ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/entity/Event.java
@@ -0,0 +1,50 @@
+package com.ticketmaster.event.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "events")
+@EntityListeners(AuditingEntityListener.class)
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "event_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "start_time", nullable = false)
+    private Time startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private Time endTime;
+
+    @Column(name = "available_seats", nullable = false)
+    private Integer availableSeats;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Instant updateAt;
+}
