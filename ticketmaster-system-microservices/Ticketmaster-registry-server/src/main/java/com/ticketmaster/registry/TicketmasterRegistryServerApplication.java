1783fe47caa89d1a19ced64640687174e7f045c5
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-09-15 06:58:48 +0300

    .

---
+++ ticketmaster-system-microservices/Ticketmaster-registry-server/src/main/java/com/ticketmaster/registry/TicketmasterRegistryServerApplication.java
@@ -0,0 +1,15 @@
+package com.ticketmaster.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class TicketmasterRegistryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketmasterRegistryServerApplication.class, args);
+	}
+
+}
---
+++ ticketmaster-system-microservices/Ticketmaster-registry-server/src/test/java/com/ticketmaster/registry/TicketmasterRegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketmaster.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmasterRegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
