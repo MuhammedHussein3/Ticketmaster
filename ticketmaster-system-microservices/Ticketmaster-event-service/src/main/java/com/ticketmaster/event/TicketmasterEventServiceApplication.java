a428987565be0665770820c5d027f416889028d4
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-09-15 07:19:32 +0300

    feat: add @EnableJpaAuditing annotation

--- ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/TicketmasterEventServiceApplication.java
+++ ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/TicketmasterEventServiceApplication.java
@@ -2,8 +2,10 @@ package com.ticketmaster.event;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TicketmasterEventServiceApplication {
 
 	public static void main(String[] args) {
