408393d76073e88db2d6a59044b3715dad558843
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-09-16 07:32:12 +0300

    refactor: Add createEvent method in EventService interface.

--- ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/service/EventService.java
+++ ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/service/EventService.java
@@ -1,6 +1,7 @@
 package com.ticketmaster.event.service;
 
-
+import com.ticketmaster.event.dto.EventCreatRequest;
+import com.ticketmaster.event.dto.EventCreateResponse;
 
 
 /**
@@ -9,4 +10,11 @@ package com.ticketmaster.event.service;
 public interface EventService {
 
 
+    /**
+     * Create a new event based on the provided request.
+     *
+     * @param eventCreatedRequest the request containing details of the event to be created.
+     * @return the response containing details of the event created.
+     */
+    EventCreateResponse createEvent(EventCreatRequest eventCreatedRequest);
 }
