b273dfb7670e8714d814a59ecac91bfe065d616b
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-09-16 09:56:55 +0300

    feat: Implement createEvent method in EventServiceImpl
    - Added the createEvent method to the EventServiceImpl class.

--- ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/service/impl/EventServiceImpl.java
+++ ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/service/impl/EventServiceImpl.java
@@ -4,11 +4,20 @@ import com.ticketmaster.event.dto.EventCreateRequest;
 import com.ticketmaster.event.dto.EventCreateResponse;
 import com.ticketmaster.event.dto.EventResponse;
 import com.ticketmaster.event.dto.EventUpdateRequest;
+import com.ticketmaster.event.entity.Category;
+import com.ticketmaster.event.entity.Event;
+import com.ticketmaster.event.entity.Venue;
 import com.ticketmaster.event.exceptions.EventCreationException;
 import com.ticketmaster.event.exceptions.EventNotFoundException;
+import com.ticketmaster.event.repository.CategoryRepository;
+import com.ticketmaster.event.repository.VenueRepository;
 import com.ticketmaster.event.service.EventService;
 import  com.ticketmaster.event.repository.EventRepository;
 import com.ticketmaster.event.mapper.EventMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
 /**
  * Implementation of the {@link EventService} interface.
  *
@@ -30,10 +39,42 @@ import com.ticketmaster.event.mapper.EventMapper;
  * @see EventRepository
  * @see EventMapper
  */
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class EventServiceImpl implements EventService {
+
+    private final EventRepository eventRepository;
+    private final CategoryRepository categoryRepository;
+    private final VenueRepository venueRepository;
+    private final EventMapper eventMapper;
+
     @Override
     public EventCreateResponse createEvent(EventCreateRequest eventCreatedRequest) throws EventCreationException {
-        return null;
+
+        Category category = categoryRepository.findById(eventCreatedRequest.categoryId()).get();
+        Venue venue = venueRepository.findById(eventCreatedRequest.venueId()).get();
+
+        Event event = Event.builder().name(eventCreatedRequest.name())
+                .availableSeats(eventCreatedRequest.availableSeats())
+                .description(eventCreatedRequest.description())
+                .startTime(eventCreatedRequest.startTime())
+                .endTime(eventCreatedRequest.endTime())
+                .category(category)
+                .venue(venue)
+                .build();
+
+        Event savedEntity = eventRepository.save(event);
+
+        return EventCreateResponse.builder()
+                .name(savedEntity.getName())
+                .description(savedEntity.getDescription())
+                .startTime(savedEntity.getStartTime())
+                .endTime(savedEntity.getEndTime())
+                .availableSeats(savedEntity.getAvailableSeats())
+                .venueId(venue.getId())
+                .categoryId(category.getId())
+                .build();
     }
 
     @Override
