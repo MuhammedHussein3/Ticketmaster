e7a2c41d9e8791e9eeaee55491cab02c588b9ed0
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-09-16 17:08:19 +0300

    feat: Add createEvent POST endpoint in EventController

--- ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/api/EventController.java
+++ ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/api/EventController.java
@@ -1,6 +1,14 @@
 package com.ticketmaster.event.api;
 
+import com.ticketmaster.event.dto.EventCreateRequest;
+import com.ticketmaster.event.dto.EventCreateResponse;
+import com.ticketmaster.event.service.EventService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EventController {
 
+    private final EventService eventService;
+
+
+    @PostMapping
+    public ResponseEntity<EventCreateResponse> createEvent(
+            @Valid @RequestBody EventCreateRequest eventCreateRequest
+            ){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(eventService.createEvent(eventCreateRequest));
+    }
 
 }
