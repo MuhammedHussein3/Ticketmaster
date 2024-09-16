e15dee5d34b13ea58a0cbf7a0bb82828ced99af2
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-09-16 10:43:24 +0300

    feat: Create EventController class for managing endpoints

---
+++ ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/api/EventController.java
@@ -0,0 +1,13 @@
+package com.ticketmaster.event.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/events")
+@RequiredArgsConstructor
+public class EventController {
+
+
+}
