d0e4768a562a3f561204a37476083e4cb6aaccee
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-09-17 20:51:44 +0300

    feat: Add Venue service interface

---
+++ ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/service/VenueService.java
@@ -0,0 +1,11 @@
+package com.ticketmaster.event.service;
+
+import com.ticketmaster.event.dto.VenueCreateRequest;
+import com.ticketmaster.event.entity.Venue;
+
+/**
+ * Service interface for managing venues
+ */
+public interface VenueService {
+
+}
