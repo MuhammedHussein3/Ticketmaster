f5b7eb2c2dd4b27dbd8dcd0a053c45ad7e19d216
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-09-17 21:34:03 +0300

    feat: Implement createVenue method in VenueServiceImpl class

---
+++ ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/service/impl/VenueServiceImpl.java
@@ -0,0 +1,41 @@
+package com.ticketmaster.event.service.impl;
+
+import com.ticketmaster.event.dto.VenueCreateRequest;
+import com.ticketmaster.event.dto.VenueResponse;
+import com.ticketmaster.event.entity.Venue;
+import com.ticketmaster.event.exceptions.VenueNotFoundException;
+import com.ticketmaster.event.mapper.VenueMapper;
+import com.ticketmaster.event.repository.VenueRepository;
+import com.ticketmaster.event.service.VenueService;
+import io.micrometer.common.util.StringUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class VenueServiceImpl implements VenueService {
+
+    private final VenueRepository venueRepository;
+    private final VenueMapper venueMapper;
+
+    @Override
+    public VenueResponse createVenue(VenueCreateRequest createRequest) {
+
+        Venue venue = mapToVenue(createRequest);
+
+        Venue savedVenue = venueRepository.save(venue);
+
+        return mapToResponse(savedVenue);
+    }
+
+    private Venue mapToVenue(VenueCreateRequest createRequest){
+        return venueMapper.mapToVenue(createRequest);
+    }
+
+    private VenueResponse mapToResponse(Venue savedVenue){
+        return venueMapper.mapToResponse(savedVenue);
+    }
+
+}
