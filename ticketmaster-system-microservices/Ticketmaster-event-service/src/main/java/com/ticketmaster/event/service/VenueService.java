3658b1bec12c47eb1489f7de39c9e374b294adb7
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-09-17 20:52:05 +0300

    feat: Add createVenue method in VenueService

--- ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/service/VenueService.java
+++ ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/service/VenueService.java
@@ -8,4 +8,15 @@ import com.ticketmaster.event.entity.Venue;
  */
 public interface VenueService {
 
+    /**
+     * Creates a new venue based on the provided request.
+     *
+     * This method initializes a new event with the details provided in the {@code createRequest},
+     * such as name, description, location, totalSeats
+     * @param createRequest the request object containing the details of the venue to be created,
+     * included name, description, location, total seats
+     * @return the response object containing details of the created venue, including the venue's ID
+     *and other information such as location.
+     */
+    Venue createVenue(VenueCreateRequest createRequest);
 }
