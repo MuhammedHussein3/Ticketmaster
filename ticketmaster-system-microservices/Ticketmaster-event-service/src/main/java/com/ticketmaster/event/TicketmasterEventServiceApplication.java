d69b3f7a1f6e00836f9e819b5358f2c4a3f64452
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-09-18 17:01:57 +0300

    feat: Add @EnableCaching annotation to TicketmasterEventServiceApplication for caching management

--- ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/TicketmasterEventServiceApplication.java
+++ ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/TicketmasterEventServiceApplication.java
@@ -2,10 +2,16 @@ package com.ticketmaster.event;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
+@EnableTransactionManagement
+@EnableCaching
 public class TicketmasterEventServiceApplication {
 
 	public static void main(String[] args) {
