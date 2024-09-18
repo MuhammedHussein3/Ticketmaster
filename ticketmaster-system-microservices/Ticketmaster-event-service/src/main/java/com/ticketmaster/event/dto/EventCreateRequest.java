f44833dd47004c0f17b26dfb91aa0ee3fd25515e
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-09-18 17:53:05 +0300

    feat: Update EventCreateRequest class with validation and serialization

--- ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/dto/EventCreateRequest.java
+++ ticketmaster-system-microservices/Ticketmaster-event-service/src/main/java/com/ticketmaster/event/dto/EventCreateRequest.java
@@ -1,18 +1,52 @@
 package com.ticketmaster.event.dto;
 
+import jakarta.validation.constraints.Min;
 import lombok.Builder;
 
-import java.sql.Time;
+import java.io.Serial;
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
 @Builder
-public record EventCreateRequest(
-        String name,
-        String description,
-        LocalDateTime startTime,
-        LocalDateTime endTime,
-        Integer availableSeats,
-        Integer venueId,
-        Integer categoryId
-) {
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class EventCreateRequest implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @NotBlank(message = "Event name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "Event description cannot be blank")
+    private String description;
+
+    @NotNull(message = "Event start time cannot be null")
+    private LocalDateTime startTime;
+
+    @NotNull(message = "Event end time cannot be null")
+    private LocalDateTime endTime;
+
+    @NotNull(message = "Available seats cannot be null")
+    @Positive(message = "Available seats must be positive")
+    @Min(1)
+    private Integer availableSeats;
+
+    @NotNull(message = "Venue ID cannot be null")
+    @Positive(message = "Venue ID must be positive")
+    private Integer venueId;
+
+    @NotNull(message = "Category ID cannot be null")
+    @Positive(message = "Category ID must be positive")
+    private Integer categoryId;
 }
+
