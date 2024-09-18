51ab4af0b07e80e3a4cb8260c0365ecc7135a62b
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-09-19 00:23:26 +0300

    Add createEvent method in SearchService

---
+++ ticketmaster-system-microservices/search-service/src/main/java/com/ticketmaster/search/service/SearchService.java
@@ -0,0 +1,20 @@
+package com.ticketmaster.search.service;
+
+import com.ticketmaster.search.dto.EventCreateRequest;
+import com.ticketmaster.search.exceptions.EventCreationException;
+/**
+ * Service interface for managing search-related operations.
+ * This service defines methods to handle event creation and indexing.
+ */
+public interface SearchService {
+
+    /**
+     * Creates a new event and indexes it for search functionality.
+     *
+     * @param eventCreateRequest the details of the event to be created,
+     *                           including name, description, start time, end time,
+     *                           available seats, venue, and category.
+     * @throws EventCreationException if the event creation process encounters any issues.
+     */
+    void createEvent(EventCreateRequest eventCreateRequest);
+}
