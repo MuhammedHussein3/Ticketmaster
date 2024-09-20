d97394ab1d8836d496a13de56ee089b4fb408350
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-09-20 19:36:13 +0300

    Add Venue class for event venue details

---
+++ ticketmaster-system-microservices/search-service/src/main/java/com/ticketmaster/search/message/Venue.java
@@ -0,0 +1,33 @@
+package com.ticketmaster.search.message;
+
+
+import lombok.*;
+
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+
+public class Venue {
+
+
+    private Integer id;
+
+
+    private String name;
+
+
+    private String description;
+
+
+    private String location;
+
+    private Integer capacity;
+
+
+    private Set<Event> events;
+}
+
