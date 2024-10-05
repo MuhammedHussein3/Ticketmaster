ee8df872fa51f47d722c02716c3f181c1c602eac
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-10-05 14:28:03 +0300

    Added unit test for createVenue

--- ticketmaster-system-microservices/event-service/src/test/java/com/ticketmaster/event/service/impl/VenueServiceImplTest.kt
+++ ticketmaster-system-microservices/event-service/src/test/java/com/ticketmaster/event/service/impl/VenueServiceImplTest.kt
@@ -25,4 +25,70 @@ import org.mockito.kotlin.whenever
 
 @ExtendWith(MockitoExtension::class)
 class VenueServiceImplTest {
+
+    @Mock
+    lateinit var venueRepository: VenueRepository
+
+    @Mock
+    lateinit var venueMapper: VenueMapper
+
+    @InjectMocks
+    lateinit var venueService: VenueServiceImpl
+
+    private lateinit var venueCreateRequest: VenueCreateRequest
+    private lateinit var venueUpdateRequest: VenueUpdateRequest
+    private lateinit var venue: Venue
+    private lateinit var venueResponse: VenueResponse
+
+    @BeforeEach
+    fun setUp() {
+        venueCreateRequest = VenueCreateRequest(
+            "Stadium A",
+            "Large Stadium",
+            "http://location-a.com",
+            10000
+        )
+
+        venue = Venue().apply {
+            id = 1
+            name = "Stadium A"
+            description = "Large Stadium"
+            location = "http://location-a.com"
+            capacity = 10000
+        }
+
+        venueUpdateRequest = VenueUpdateRequest(
+            "Stadium B",
+            "Small Stadium",
+            "http://location-b.com",
+            5000
+        )
+
+        venueResponse = VenueResponse(
+            1,
+            "Stadium A",
+            "Large Stadium",
+            "http://location-a.com",
+            10000
+        )
+    }
+
+    @Test
+    fun `test createVenue success`() {
+        whenever(venueMapper.mapToVenue(venueCreateRequest)).thenReturn(venue)
+        whenever(venueRepository.save(venue)).thenReturn(venue)
+        whenever(venueMapper.mapToResponse(venue)).thenReturn(venueResponse)
+
+        val result = venueService.createVenue(venueCreateRequest)
+
+        assertNotNull(result)
+        assertEquals("Stadium A", result.name)
+        assertEquals(10000, result.totalSeats)
+        assertEquals("http://location-a.com", result.location)
+        assertEquals("Large Stadium", result.description)
+
+        verify(venueRepository, times(1)).save(venue)
+    }
+
+
 }
