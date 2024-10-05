b0abdc54732ef97920f5e3b72fcd69786ed7e8c6
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-10-05 15:36:35 +0300

    Add unit test for createCategory method in CategoryServiceImpl

--- ticketmaster-system-microservices/event-service/src/test/java/com/ticketmaster/event/service/impl/CategoryServiceImplTest.kt
+++ ticketmaster-system-microservices/event-service/src/test/java/com/ticketmaster/event/service/impl/CategoryServiceImplTest.kt
@@ -21,4 +21,47 @@ import org.mockito.Mockito.*
 
 @ExtendWith(MockitoExtension::class)
 class CategoryServiceImplTest {
+
+    @Mock
+    private lateinit var categoryRepository: CategoryRepository
+
+    @Mock
+    private lateinit var categoryMapper: CategoryMapper
+
+    @InjectMocks
+    private lateinit var categoryService: CategoryServiceImpl
+
+    private lateinit var categoryRequest: CategoryRequest
+    private lateinit var category: Category
+
+    @BeforeEach
+    fun setUp() {
+        categoryRequest = CategoryRequest(
+            "Sport",
+             "Sport category description"
+        )
+
+        category = Category().apply {
+            id = 1
+            name = "Sport"
+            description = "Sport category description"
+        }
+
+
+    }
+
+    @Test
+    fun `createCategory should save and return category successfully`() {
+        `when`(categoryMapper.mapToCategory(categoryRequest)).thenReturn(category)
+        `when`(categoryRepository.save(category)).thenReturn(category)
+
+        val result = categoryService.createCategory(categoryRequest)
+
+        assertNotNull(result)
+        assertEquals("Sport", result.name)
+        assertEquals("Sport category description", result.description)
+
+        verify(categoryRepository, times(1)).save(category)
+    }
+
 }
